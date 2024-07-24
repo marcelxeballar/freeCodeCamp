c762b96c4d012fa9a77f31fbc5bdc1856347f8da
MARCEL <marcel.eballar@icloud.com>
2024-07-23 23:57:21 -0400

    feat: Implement results-div functionality

--- Number-Validator/scripts.js
+++ Number-Validator/scripts.js
@@ -1,35 +1,44 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 const countrySelected = document.querySelector(".ct-selected");
 const countryList  = document.querySelector(".ct-list");
-const tip = document.querySelector(".tip-container");
+const tipDiv = document.querySelector(".tip-container");
+const resultDiv = document.getElementById("results-div")
+
 const input = document.getElementById("user-input");
-const checkButton = document.getElementById("check-btn")
+const deleteInputBtn= document.querySelector(".delete-input");
+const checkButton = document.getElementById("check-btn");
+let countryID = "US";
 
 const countryInfo = {
   us: {
     name: "United States",
     flag: "https://cdn0.iconfinder.com/data/icons/195-flat-flag-psd-icons/70/United-States-of-America.png",
+    placeholder: "+1 (AAA) BB-CCCC"
   },
   br: {
     name: "Brazil",
     flag: "https://cdn0.iconfinder.com/data/icons/195-flat-flag-psd-icons/70/Brazil.png",
+    placeholder: "+55 (AA) BBBBB-CCCC"
   },
   ph: {
     name: "Philippines",
     flag: "https://cdn0.iconfinder.com/data/icons/195-flat-flag-psd-icons/70/Philippines.png",
+    placeholder: "+63 (AA) BBBB CCCC"
   },
   icons: {
     info: `info`,
-    wrong: `dangerous`,
+    failed: `dangerous`,
     correct: `verified`
   },
   message: {
-    us: "Type a US phone number as (123) 456-7890.",
-    br: "Type a Brazilian phone number as +55 11 91234-5678.",
-    ph: "Type a Philippine phone number as +63 912 345 6789.",
     error: "OPPS! It seems you did not provide a number."
   },
 }
+
+countryInfo.message.us = `Type a US phone number as ${countryInfo.us.placeholder}.`;
+countryInfo.message.br = `Type a BR phone number as ${countryInfo.br.placeholder}.`;
+countryInfo.message.ph = `Type a PH phone number as ${countryInfo.ph.placeholder}.`;
+countryInfo.message.error = "OPPS! It seems you did not provide a number.";
 
 countryList.innerHTML = `
   <div class="flex-row countries" id="us">
@@ -54,53 +63,96 @@ function updateCountrySelected(country){
   <img src=${countryInfo[country].flag}>
   <p>${countryInfo[country].name}</p>
 `;
-}
+};
 
-function updateTip(background, iconKey, typeOfMessage){
-  tip.style.background = background;
-  tip.innerHTML = `
-  <p class="flex-row">
+function updateTip(background, iconKey, tipMessage){
+  tipDiv.style.background = background;
+  tipDiv.innerHTML = `
   <span class="material-symbols-outlined">${countryInfo.icons[iconKey]}</span>
-  ${countryInfo.message[typeOfMessage]}
-  </p>
+  <p id="tip-message">${countryInfo.message[tipMessage]}</p>
 `;
+};
+
+function updateResult(background, iconKey, tipMessage){
+  resultDiv.style.background = background;
+  resultDiv.innerHTML = `
+  <span class="material-symbols-outlined">${countryInfo.icons[iconKey]}</span>
+  <p id="result-message">${countryInfo.message[tipMessage]}</p>
+`;
+};
+
+const toggleInputDlt = () => {
+  if (input.value.length > 0){
+    deleteInputBtn.style.display = "block";
+  }
+  else {
+    deleteInputBtn.style.display = "none";
+  }
 }
 
 /* Runs after the innerHTML is loaded. */
 /* Runs after the innerHTML is loaded. */
 
+//Display us-selected 
   updateCountrySelected("us");
   updateTip("#895212", "info", "us")
+  updateResult("", "", "");
   
+  //Shows Country-list
   countrySelected.addEventListener("click", (event) => {
     event.stopPropagation();
     countryList.classList.toggle("hidden");
   });
 
+  //Hides Countrylist
  document.addEventListener("click", () => {
   if(!countryList.classList.contains("hidden")){
     countryList.classList.toggle("hidden");
   }
  })
 
+  //Changes selected country
   const countries =  document.querySelectorAll(".countries");
   countries.forEach(function(country) {
     country.addEventListener("click", function(){
-      console.log(country.id)
       updateCountrySelected(`${country.id}`);
       countryList.classList.toggle("hidden");
       updateTip("#895212", "info", country.id)
-      countryListIsOpen = true;
+      input.placeholder = countryInfo[country.id].placeholder;
+      input.value = "";
+      countryID = country.id.toUpperCase();
     })
-  })
+  });
+
+  //Toggle the trash can icon.
+  input.addEventListener("keyup", (event) =>{
+    toggleInputDlt()
+    console.log(input.value.length)
+  });
+
+  deleteInputBtn.addEventListener("click", () => {
+    input.value = "";
+    toggleInputDlt()
+  });
+
+  //Changes Tip
 
   checkButton.addEventListener("click", () => {
     if (input.value === "" && input.value.length === 0){
       alert("Please provide a phone number");
-      updateTip("#b62424", "wrong", "error");
+      updateTip("#b62424", "failed", "error");
     }
     else {
-      console.log(input.value)
+      numberValidation(input.value)
+    };
+  });
+
+  function numberValidation(number){
+    if (/^[0-9()+\s-]*$/.test(number)){
+      console.log(true);
     }
-  })
+    else {
+      deleteInputBtn.style.display = "none"; 
+    }
+  }
 });
